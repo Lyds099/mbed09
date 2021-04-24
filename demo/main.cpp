bbc9b137512c898df52bdcc0cb29193ca3636951
blair488 <samantha99fan@gmail.com>
2021-04-24 23:09:58 +0800

    add main.cpp

---
+++ demo/main.cpp
@@ -0,0 +1,58 @@
+#include "mbed.h"
+#include "mbed_rpc.h"
+
+/**
+ *  This example program has been updated to use the RPC implementation in the new mbed libraries.
+ *  This example demonstrates using RPC over serial
+**/
+RpcDigitalOut myled(LED3,"myled");
+BufferedSerial pc(USBTX, USBRX);
+void LEDControl(Arguments *in, Reply *out);
+RPCFunction rpcLED(&LEDControl, "LEDControl");
+double x;
+
+int main() {
+    //The mbed RPC classes are now wrapped to create an RPC enabled version - see RpcClasses.h so don't add to base class
+
+    // receive commands, and send back the responses
+    char buf[256], outbuf[256];
+
+    FILE *devin = fdopen(&pc, "r");
+    FILE *devout = fdopen(&pc, "w");
+
+    while(1) {
+        memset(buf, 0, 256);
+        for (int i = 0; ; i++) {
+            char recv = fgetc(devin);
+            if (recv == '\n') {
+                printf("\r\n");
+                break;
+            }
+            buf[i] = fputc(recv, devout);
+        }
+        //Call the static call method on the RPC class
+        RPC::call(buf, outbuf);
+        printf("%s\r\n", outbuf);
+    }
+}
+
+// Make sure the method takes in Arguments and Reply objects.
+void LEDControl (Arguments *in, Reply *out)   {
+    bool success = true;
+
+    // In this scenario, when using RPC delimit the two arguments with a space.
+    x = in->getArg<double>();
+    int on = x;
+
+    // Have code here to call another RPC function to wake up specific led or close it.
+    char buffer[200], outbuf[256];
+    char strings[20];
+    sprintf(strings, "/myled/write %d", on);
+    strcpy(buffer, strings);
+    RPC::call(buffer, outbuf);
+    if (success) {
+        out->putData(buffer);
+    } else {
+        out->putData("Failed to execute LED control.");
+    }
+}
